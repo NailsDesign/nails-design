b459db4530994fa02297d59541bf7da3aa469e89
NailsDesign <admin@nailsdesign.co.uk>
2025-06-26 02:32:43 +0100

    next-sitemap.config

--- frontend/next-sitemap.config.cjs
+++ frontend/next-sitemap.config.cjs
@@ -1,5 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://nailsdesign.co.uk',
+  siteUrl: 'https://nails-design.vercel.app/',
   generateRobotsTxt: true,
 };
